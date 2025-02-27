27316efc4f49462d941ac080d8865381253c7fd2
Krzysztof Zych <k33rni@gmail.com>
2025-02-27 11:31:18 +0100

    Allow filtering photo by source article type, default to all

--- static/index.js
+++ static/index.js
@@ -8,6 +8,12 @@ class SlideshowController {
     slide_change_time = 10000 // milliseconds = 10 seconds
     autoadvance = null
 
+    allow_event = true
+    allow_talent = true
+    allow_venue = true
+    allow_championship = true
+    allow_article = true
+
     constructor(root, start) {
         this.element = root
         this.img_element = root.querySelector('.pic')
@@ -52,14 +58,25 @@ class SlideshowController {
             .catch(error => console.error('Error:', error))
     }
 
-    start_slideshow() {
-        const speed_input = document.querySelector('[name=speed]:checked')
+    read_controls() {
+        const $ = (sel) => document.querySelector(sel)
+        const speed_input = $('[name=speed]:checked')
         if (speed_input) {
             this.slide_change_time = Number(speed_input.value)
             console.log("SCT", this.slide_change_time)
         }
 
-        this.display_progress = !!document.querySelector('[name=progressbar_visible]:checked')
+        this.display_progress = !!$('[name=progressbar_visible]:checked')
+
+        this.allow_talent = !!$('[name=filter][value=talent]:checked')
+        this.allow_event = !!$('[name=filter][value=event]:checked')
+        this.allow_venue = !!$('[name=filter][value=venue]:checked')
+        this.allow_championship = !!$('[name=filter][value=championship]:checked')
+        this.allow_article = !!$('[name=filter][value=article]:checked')
+    }
+
+    start_slideshow() {
+        this.read_controls()
 
         this.element.requestFullscreen()
             .then(() => this.advance_and_reset_interval())
@@ -74,7 +91,16 @@ class SlideshowController {
     }
 
     advance_slide() {
-        this.current_slide = (this.current_slide + 1) % this.all_photos.length
+        while (true) {
+            this.current_slide = (this.current_slide + 1) % this.all_photos.length
+            const [_, photo] = this.all_photos[this.current_slide]
+            if (photo.event && this.allow_event) break
+            if (photo.venue && this.allow_venue) break
+            if (photo.talent && this.allow_talent) break
+            if (photo.championship && this.allow_championship) break
+            if (photo.article && this.allow_article) break
+        }
+
         const [key, photo] = this.all_photos[this.current_slide]
         const path = `${this.gallery_root}${photo.path}`
         const img_element = this.use_spare ? this.img_element : this.spare_element
@@ -185,233 +211,6 @@ class SlideshowController {
 
 
 
-}
-
-class NameFinderController {
-    constructor(root, orgs_controller) {
-        this.element = root
-        this.nameTarget = root.querySelector('input[type=text]')
-        this.matchesTarget = root.querySelector('ul#matches')
-        this.headerTemplateTarget = root.querySelector('template#card-header')
-        this.matchTemplateTarget = root.querySelector('template#card-match')
-        this.orgs = orgs_controller
-        this.connect()
-    }
-    connect() {
-        this.populate_completion_list()
-        this.load_matches()
-        this.nameTarget.value = ''
-        this.nameTarget.attributes.tabIndex = '1'
-        const form = this.element.querySelector('form')
-        form.addEventListener('submit', (event) => event.preventDefault())
-        form.addEventListener('click', this.handle_buttons.bind(this))
-        this.add_input_handlers(this.nameTarget)
-        marked.use({ hooks: { postprocess: this.postprocess_html.bind(this) } })
-    }
-
-    add_input_handlers(element) {
-    }
-
-    ac_query(text) {
-        const lc = text.toLowerCase()
-        return this.name_list.filter(name => name.toLowerCase().includes(lc))
-    }
-
-    add_more() {
-        const row = this.element.querySelector(".name-row:first-child")
-        const clone = row.cloneNode(true)
-        const input = clone.querySelector('input[type=text]')
-        input.value = ''
-        row.parentNode.insertAdjacentElement("beforeend", clone)
-        this.add_input_handlers(input)
-    }
-
-    del_row(event) {
-        const row = event.target.closest(".name-row")
-        row.parentNode.removeChild(row)
-        this.lookup()
-    }
-
-    handle_buttons(event) {
-        if (event.target.tagName != 'BUTTON') return
-        if (event.target.className == 'add-name')
-            this.add_more()
-        else if (event.target.className == 'remove')
-            this.del_row(event)
-    }
-
-    async load_matches() {
-        const path = this.element.dataset.matches
-        fetch(path)
-            .then(response => response.json())
-            .then(data => {
-                this.all_matches = data
-            })
-            .catch(error => console.error('Error:', error))
-    }
-
-    async populate_completion_list() {
-        const path = this.element.dataset.appearances
-        fetch(path)
-            .then(response => response.json())
-            .then(data => {
-                this.appearances = data
-                this.name_list = Object.keys(data)
-            })
-            .catch(error => console.error('Error:', error))
-    }
-
-    all_names() {
-        const inputs = this.element.querySelectorAll('input[type=text]')
-        return Array.prototype.map.call(inputs, (el) => el.value)
-    }
-
-    lookup() {
-        const names = this.all_names()
-        const first = names.shift()
-        const matches = this.appearances[first]
-        if (!matches) {
-            this.empty_result()
-            return
-        }
-        let indices = matches.map(el => el[0])
-
-        for (const other of names) {
-            if (other == '') continue
-            let other_matches = this.appearances[other]
-            if (!other_matches) {
-                this.empty_result()
-                return
-            }
-            let other_indices = other_matches.map(el => el[0])
-            indices = indices.filter(value => other_indices.includes(value))
-        }
-
-        if (indices.length == 0) {
-            this.empty_result()
-            return
-        }
-        this.show_matches(indices)
-    }
-
-    empty_result() {
-        const header = this.headerTemplateTarget.content.cloneNode(true)
-        this.matchesTarget.innerHTML = ''
-        this.matchesTarget.appendChild(header)
-    }
-
-    show_matches(indices) {
-        const header = this.headerTemplateTarget.content.cloneNode(true)
-        this.matchesTarget.innerHTML = ''
-        this.matchesTarget.appendChild(header)
-        const match_template = this.matchTemplateTarget.content
-
-        const matches = indices.map((index) => structuredClone(this.all_matches[index]))
-        matches.sort((a, b) => new Date(b.d) - new Date(a.d))
-
-        for (const match of matches) {
-            const t = match_template.cloneNode(true)
-
-            const d = t.querySelector('.d')
-            d.innerHTML = match['d']
-            d.datetime = match['d']
-
-            const o = t.querySelector('.o')
-            for (const org of match['o']) {
-                o.appendChild(this.orgs.get_badge(org))
-            }
-
-            const r = t.querySelector('.r')
-            r.innerHTML = this.format_details(match)
-
-            this.matchesTarget.appendChild(t)
-        }
-    }
-
-    format_details(match) {
-        let event_name = match.n
-        let participants = match.m
-        const { length, [length - 1]: last } = participants
-        let options
-
-        if (typeof last === 'object') {
-            options = last
-            participants.pop()
-        } else
-            options = {}
-
-        let head = ''
-        let participant_sep = ' and '
-        let sep = options.nc ? ' vs ' : ' defeated '
-        let end = options.nc ? ` - ${options.nc}` : options.r ? ` via ${options.r}` : ''
-        if (end == '' && new Date(match.d) > new Date()) {
-            end = ' - upcoming'
-            sep = ' vs '
-        }
-        if (options.g) {
-            participant_sep = ", "
-            sep = ', '
-            head = '<strong>Segment: </strong>'
-        }
-
-        const winner = participants.shift()
-        const matchtype = options.s || 'Singles Match'
-        const title = options.c || ''
-
-        let text = head
-        text += this.format_names(winner)
-        if (participants.length > 0)
-            text += sep
-        text += participants.map((name) => this.format_names(name)).join(participant_sep)
-        text += end
-        text += '<br/>'
-
-        if (title)
-            text += `<strong>${marked.parseInline(title)}</strong> `
-        text += `<strong>${marked.parseInline(matchtype)}</strong> at `
-        text += this.format_link(event_name, match.p)
-
-        return text
-    }
-
-    format_names(names) {
-        // Split names on plus, comma or semicolon, then pass each name to format_name and join with the original symbols
-        const splitNames = names.split(/(\s+(?:\+|,|;)\s+)/);
-        return splitNames.map((part) => {
-            // Only format the parts that are names, not the delimiters
-            if (part.match(/(\s+(?:\+|,|;)\s+)/)) {
-                return part;
-            } else {
-                return marked.parseInline(part)
-            }
-        }).join('');
-    }
-
-
-    format_link(title, target) {
-        return marked.parseInline(`[${title}](@/${target})`)
-    }
-}
-
-class OrgsController {
-    constructor(root) {
-        this.element = root
-    }
-    get_badge(org) {
-        const id = `#org-badge-${org}`
-        const el = this.element.querySelector(id)
-        if (el)
-            return el.content.cloneNode(true)
-
-        let badge = document.getElementById('org-badge-zzz').content.cloneNode(true)
-        badge.querySelector('a.org-badge span').textContent = org.toUpperCase()
-        badge.querySelector('a.org-badge').href = this.org_link(org)
-        return badge
-    }
-
-    org_link(org) {
-        return `//tpwres.pl/o/${org}`
-    }
 }
 
 const root = document.querySelector('#slideshow-container')
